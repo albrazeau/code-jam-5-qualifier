850b3f048eb89f8a71601fdcf5d830149711e2b3
albrazeau <abrazeau1993@gmail.com>
2019-06-10 22:47:59 -0400

    made shorter. next: split into multiple functions

--- qualifier.py
+++ qualifier.py
@@ -1,11 +1,12 @@
 from random import choice
 
+
 def generate_password(
     password_length: int = 8,
     has_symbols: bool = False,
     has_uppercase: bool = False,
-    ignored_chars : list = [],
-    allowed_chars : list = []
+    ignored_chars: list = [],
+    allowed_chars: list = [],
 ) -> str:
     """Generates a random password.
 
@@ -17,56 +18,47 @@ def generate_password(
     `allowed_chars` and `ignored_chars` cannot be used together
     """
     # Check for correct usage
-    assert password_length < 1000000, '`password_length` must be < 1,000,000 chars'
-    
+    assert password_length < 1000000, "`password_length` must be < 1,000,000 chars"
+
     # make sure both `allowed_chars` and `ignored_chars` are not being used
     if ignored_chars:
         if allowed_chars:
-            raise UserWarning('Using both `allowed_chars` and `ignored_chars` is not allowed')
-    
-    # make sure that the booling conditions are met if using allowed_chars
+            raise UserWarning(
+                "Using both `allowed_chars` and `ignored_chars` is not allowed"
+            )
+
+    # make sure that the boolean conditions are met if using allowed_chars
     if allowed_chars:
         if has_symbols:
             for c in allowed_chars:
-                assert c in has_all_params['symbols'], 'Error, mismatched arguments. You want a symbol but there is not a symbol in `allowed_chars`'
+                assert (
+                    c in has_all_params["symbols"]
+                ), "Error, mismatched arguments. You want a symbol but there is not a symbol in `allowed_chars`"
         if has_uppercase:
             for c in allowed_chars:
-                assert c in has_all_params['uppers'], 'Error, mismatched arguments. You want a uppercase but there is not a uppercase in `allowed_chars`'
+                assert (
+                    c in has_all_params["uppers"]
+                ), "Error, mismatched arguments. You want a uppercase but there is not a uppercase in `allowed_chars`"
 
-    has_all_params = {
-        'lowers':'abcdefghijklmnopqrstuvwxyz',
-        'uppers': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-        'symbols': '''`~!@#$%^&*()-_=+[{]}\|/?.>,<;:'"]'''
-    }
-    
-    has_sym_params = {
-        'lowers':'abcdefghijklmnopqrstuvwxyz',
-        'symbols': '''`~!@#$%^&*()-_=+[{]}\|/?.>,<;:'"]'''
-    }
-    
-    has_up_params = {
-        'lowers':'abcdefghijklmnopqrstuvwxyz',
-        'uppers': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    }
-    
-    has_none_params = {
-        'lowers':'abcdefghijklmnopqrstuvwxyz',
-    }
-    
+    lowers = "abcdefghijklmnopqrstuvwxyz"
+    uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    symbols = """`~!@#$%^&*()-_=+[{]}\|/?.>,<;:'"]"""
+    upchoice = lowers + uppers
+    symchoice = lowers + symbols
+    allchoice = lowers + uppers + symbols
+
     # has only symbols and lowers
     if has_symbols:
         if not has_uppercase:
-            password = ''
-            while not any(char in has_all_params['symbols'] for char in password):
-                password = ''
+            password = ""
+            while not any(char in symchoice for char in password):
+                password = ""
                 if ignored_chars:
                     for c in ignored_chars:
-                        has_sym_params['lowers'] = has_sym_params['lowers'].replace(c, '')
-                        has_sym_params['symbols'] = has_sym_params['symbols'].replace(c, '')
+                        symchoice = symchoice.replace(c, "")
 
-                    for i in range(password_length):            
-                        chartype = choice(list(has_sym_params.keys()))
-                        char = choice(has_sym_params[chartype])
+                    for i in range(password_length):
+                        char = choice(symchoice)
                         password += char
 
                 elif allowed_chars:
@@ -74,26 +66,23 @@ def generate_password(
                         char = choice(allowed_chars)
                         password += char
 
-                else: 
+                else:
                     for i in range(password_length):
-                        chartype = choice(list(has_sym_params.keys()))
-                        char = choice(has_sym_params[chartype])
+                        char = choice(symchoice)
                         password += char
-    
+
     # has only uppers and lowers
     if has_uppercase:
         if not has_symbols:
-            password = ''
-            while not any(char in has_all_params['uppers'] for char in password):
-                password = ''
+            password = ""
+            while not any(char in upchoice for char in password):
+                password = ""
                 if ignored_chars:
                     for c in ignored_chars:
-                        has_up_params['uppers'] = has_up_params['uppers'].replace(c, '')
-                        has_up_params['lowers'] = has_up_params['lowers'].replace(c, '')
+                        upchoice = upchoice.replace(c, "")
 
-                    for i in range(password_length):            
-                        chartype = choice(list(has_up_params.keys()))
-                        char = choice(has_up_params[chartype])
+                    for i in range(password_length):
+                        char = choice(upchoice)
                         password += char
 
                 elif allowed_chars:
@@ -101,27 +90,23 @@ def generate_password(
                         char = choice(allowed_chars)
                         password += char
 
-                else: 
+                else:
                     for i in range(password_length):
-                        chartype = choice(list(has_up_params.keys()))
-                        char = choice(has_up_params[chartype])
+                        char = choice(upchoice)
                         password += char
-    
+
     # has symbols, uppers and lowers
     if has_symbols:
         if has_uppercase:
-            check=True
+            check = True
             while check:
-                password = ''
+                password = ""
                 if ignored_chars:
                     for c in ignored_chars:
-                        has_all_params['uppers'] = has_all_params['uppers'].replace(c, '')
-                        has_all_params['lowers'] = has_all_params['lowers'].replace(c, '')
-                        has_all_params['symbols'] = has_all_params['symbols'].replace(c, '')
+                        allchoice = allchoice.replace(c, "")
 
-                    for i in range(password_length):            
-                        chartype = choice(list(has_all_params.keys()))
-                        char = choice(has_all_params[chartype])
+                    for i in range(password_length):
+                        char = choice(allchoice)
                         password += char
 
                 elif allowed_chars:
@@ -129,25 +114,24 @@ def generate_password(
                         char = choice(allowed_chars)
                         password += char
 
-                else: 
+                else:
                     for i in range(password_length):
-                        chartype = choice(list(has_all_params.keys()))
-                        char = choice(has_all_params[chartype])
+                        char = choice(allchoice)
                         password += char
-                if any(char in has_all_params['symbols'] for char in password):
-                    if any(char in has_all_params['uppers'] for char in password):
+
+                if any(char in uppers for char in password):
+                    if any(char in symbols for char in password):
                         check = False
-    
+
     # has only lowercase
     else:
-        password = ''
+        password = ""
         if ignored_chars:
             for c in ignored_chars:
-                has_none_params['lowers'] = has_none_params['lowers'].replace(c, '')
+                lowers = lowers.replace(c, "")
 
-            for i in range(password_length):            
-                chartype = choice(list(has_none_params.keys()))
-                char = choice(has_none_params[chartype])
+            for i in range(password_length):
+                char = choice(lowers)
                 password += char
 
         elif allowed_chars:
@@ -155,10 +139,9 @@ def generate_password(
                 char = choice(allowed_chars)
                 password += char
 
-        else: 
+        else:
             for i in range(password_length):
-                chartype = choice(list(has_none_params.keys()))
-                char = choice(has_none_params[chartype])
+                char = choice(lowers)
                 password += char
-    
+
     return password
